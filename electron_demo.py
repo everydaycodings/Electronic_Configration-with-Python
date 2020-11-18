7f57954ae32cbe7ed8389bc77616c00e04b8b20f
Kumar Saksham <everydaycodings@gmail.com>
2020-11-18 19:22:28 +0530

    added element name and its atomic number

--- electron_demo.py
+++ electron_demo.py
@@ -1,4 +1,28 @@
-elem = int(input("Enter the atomic number of element: "))
+user = input("Enter the name of the element: ")
+
+element = {"hydrogen": 1, "helium": 2, "lithium": 3, "berillium": 4, "boron": 5,
+           "carbon": 6, "nitrogen": 7, "oxygen": 8, "fluorine": 9, "neon": 10,
+           "sodium": 11, "magnesium": 12, "aluminium": 13, "silicon": 14, "phosporus":15,
+           "sulphur": 16, "chlorine": 17, "argon": 18, "potassium": 19, "calcium": 20,
+           "scandium": 21, "titanium": 22, "vanadium": 23, "chromium": 24, "manganese": 25,
+           "iron": 26, "cobalt": 27, "nickel": 28, "copper": 29, "zinc": 30, "gallium": 31,
+           "germanium": 32, "arsenic": 33, "selenium": 34, "bromine": 35, "krypten": 36,
+           "rubidium": 37, "strontium": 38, "yttrium": 39, "zirconium": 40, "niobium": 41,
+           "molybdenum": 42, "technetium": 43, "ruthenium": 44, "rhodium": 45, "palladium": 46,
+           "silver": 47, "cadmium": 48, "indium": 49, "tin": 50, "antimony": 51, "tellurium": 52,
+           "iodine": 53, "xenon": 54, "caesium": 55, "barium": 56, "lanthenum": 57, "cerium": 58,
+           "praseodymium": 59,  "neodymium": 60, "promethium": 61, "samarium": 62, "europium": 63,
+           "gadolinium": 64, "terbium": 65, "dysprosium": 66, "holmium": 67, "erbium": 68, "thulium": 69,
+           "yetterbium": 70, "lutetium": 71, "hafnium": 72, "tantalum": 73, "tungsten": 74, "rehnium": 75,
+           "osmium": 76, "iridium": 77, "platinum": 78, "gold": 79, "mercury": 80, "thalium": 81,"lead": 82,
+           "bismuth": 83, "polonium": 84, "astatine": 85, "radon": 86, "francium": 87, "radium": 88, 
+           "actinium": 89, "thorium": 90, "protoactinium": 91, "uranium": 92, "neptunium": 93, "plutonium": 94,
+           "americium": 95, "curium": 96, "barkelium": 97, "californium": 98, "einsteinium": 99, "fermium": 100,
+           "mendelevium": 101, "nobelium": 102, "lawrencium": 103
+
+}
+
+elem = element.get(user)
 
 eleconfig = ""
 prefix = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
@@ -27,5 +51,6 @@ for x in range(len(prefix)):
         eleconfig = eleconfig + prefix[x] + str(elem)
         break
     print(elem)
+
 
 print(eleconfig)
