5666a1a31af1874c931b9d19370d3d8a8d80fc0e
Kumar Saksham <everydaycodings@gmail.com>
2020-11-18 13:17:59 +0530

    added electron_demo.py file for the base code of this project to find electronic configration of a perticular element

---
+++ electron_demo.py
@@ -0,0 +1,29 @@
+elem = int(input("Enter the atomic number of element: "))
+
+eleconfig = ""
+prefix = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
+
+def maxocc(s):
+    global maxoccu
+    if s[1] == "s":
+        maxoccu = 2
+    if s[1] == "p":
+        maxoccu = 6
+    if s[1] == "d":
+        maxoccu = 10
+    if s[1] == "f":
+        maxoccu = 14
+
+for x in range(len(prefix)):
+    maxocc(prefix[x])
+    if elem == 0:
+        break
+    elif elem >= maxoccu:
+        eleconfig = eleconfig + prefix[x] + str(maxoccu) + " "
+        elem = elem - maxoccu
+    elif elem < maxoccu:
+        eleconfig = eleconfig + prefix[x] + str(elem)
+        break
+    print(elem)
+
+print(eleconfig)
