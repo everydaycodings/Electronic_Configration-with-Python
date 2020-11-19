85512ea2f6d9c5f80dcf032f33d4da42e86e032a
Kumar Saksham <everydaycodings@gmail.com>
2020-11-19 12:01:49 +0530

    added try except block

--- electron_demo.py
+++ electron_demo.py
@@ -21,35 +21,42 @@ element = {"hydrogen": 1, "helium": 2, "lithium": 3, "berillium": 4, "boron": 5,
            "mendelevium": 101, "nobelium": 102, "lawrencium": 103
 
 }
+try:
+    elem = element.get(user)
 
-elem = element.get(user)
 
-eleconfig = ""
-prefix = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
+    eleconfig = ""
+    prefix = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
 
-def maxocc(s):
-    global maxoccu
-    if s[1] == "s":
-        maxoccu = 2
-    if s[1] == "p":
-        maxoccu = 6
-    if s[1] == "d":
-        maxoccu = 10
-    if s[1] == "f":
-        maxoccu = 14
+    def maxocc(s):
+        global maxoccu
+        if s[1] == "s":
+            maxoccu = 2
+        if s[1] == "p":
+            maxoccu = 6
+        if s[1] == "d":
+            maxoccu = 10
+        if s[1] == "f":
+            maxoccu = 14
 
-for x in range(len(prefix)):
-    maxocc(prefix[x])
-    if elem == 0:
-        break
-    elif elem >= 118:
-        break
-    elif elem >= maxoccu:
-        eleconfig = eleconfig + prefix[x] + str(maxoccu) + " "
-        elem = elem - maxoccu
-    elif elem < maxoccu:
-        eleconfig = eleconfig + prefix[x] + str(elem)
-        break
-    print(elem)
+    for x in range(len(prefix)):
+        maxocc(prefix[x])
+        if elem == 0:
+            break
+        elif elem >= 118:
+            break
+        elif elem >= maxoccu:
+            eleconfig = eleconfig + prefix[x] + str(maxoccu) + " "
+            elem = elem - maxoccu
+        elif elem < maxoccu:
+            eleconfig = eleconfig + prefix[x] + str(elem)
+            break
 
-print(eleconfig.split())
+    list_eleconfig = eleconfig.split()
+    print(list_eleconfig)
+
+    print(list_eleconfig[-1])
+    print(list_eleconfig[-2])
+
+except TypeError:
+    print("wrong element")
